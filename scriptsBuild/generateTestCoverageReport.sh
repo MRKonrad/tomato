fb8a603124661efbad9562fe3f6bc64959a8b574
Konrad Werys <konradwerys2@gmail.com>
2019-07-30 13:13:30 +0200

    generateTestCoverageReport fix

--- scriptsBuild/generateTestCoverageReport.sh
+++ scriptsBuild/generateTestCoverageReport.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-lcov --capture --directory . --output-file coverage_full.info
-lcov --remove coverage_full.info '*/thirdParty/*' '*/tests/*' '*/include/ITK*' '*v1*' '*/ITKIOFactoryRegistration/*' '*/usr/include/*' -o coverage.info
-genhtml coverage.info --output-directory out
-open out/index.html
+eval lcov --capture --directory . --output-file coverage_full.info
+eval lcov --remove coverage_full.info '*/thirdParty/*' '*/tests/*' '*/include/ITK*' '*v1*' '*/ITKIOFactoryRegistration/*' '*/usr/include/*' -o coverage.info
+eval genhtml coverage.info --output-directory out
+eval open out/index.html
