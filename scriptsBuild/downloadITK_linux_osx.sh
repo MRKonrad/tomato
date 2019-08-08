6db99da460ff3a0a7ad8e97f307dc95548deec57
Konrad Werys <konradwerys2@gmail.com>
2019-08-08 16:44:08 +0200

    Build: fixing paths

--- scriptsBuild/downloadITK_linux_osx.sh
+++ scriptsBuild/downloadITK_linux_osx.sh
@@ -14,7 +14,7 @@ cd ../..
 mkdir ITK_install
 cd ITK_install
 curl -L -O https://github.com/MRKonrad/build-itk/releases/download/itk_v4.13.2/itk_v4.13.2_${mysystem}_g++_CXX98.zip
-unzip -a -q itk_v4.13.2_${mysystem}_.zip
-rm itk_v4.13.2_${mysystem}_.zip
+unzip -a -q itk_v4.13.2_${mysystem}_g++_CXX98.zip
+rm itk_v4.13.2_${mysystem}_g++_CXX98.zip
 
 echo "ITK binaries downloaded"
