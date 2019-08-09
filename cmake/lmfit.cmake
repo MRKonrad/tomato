41f675a3cc5f79705531204f93deb88aca428611
Konrad Werys <konradwerys2@gmail.com>
2019-08-09 11:23:55 +0200

    build: adding lmfit

--- cmake/lmfit.cmake
+++ cmake/lmfit.cmake
@@ -1,12 +1,17 @@
 if(USE_LMFIT)
-    add_subdirectory(thirdParty/lmfit)
-#    include_directories (${lmfit_SOURCE_DIR}/lib)
-#    set(TOMATO_LIBS_TO_LINK ${TOMATO_LIBS_TO_LINK} lmfit)
-#    set(LIB_INSTALL OFF)
-#    set(LIB_MAN OFF)
-#    set(FITTEST OFF)
-#    install (TARGETS lmfit
-#            RUNTIME DESTINATION ${CMAKE_INSTALL_PREFIX}/${LIB_INSTALL_DIR}
-#            LIBRARY DESTINATION ${CMAKE_INSTALL_PREFIX}/${LIB_INSTALL_DIR}
-#            ARCHIVE DESTINATION ${CMAKE_INSTALL_PREFIX}/${LIB_INSTALL_DIR})
+
+    # this way brings unexplained cmake errors
+    #    add_subdirectory(thirdParty/lmfit)
+    #    include_directories (${lmfit_SOURCE_DIR}/lib)
+    #    set(TOMATO_LIBS_TO_LINK ${TOMATO_LIBS_TO_LINK} lmfit)
+
+    include_directories ("thirdParty/lmfit/lib")
+    set(src_files
+            "thirdParty/lmfit/lib/lmcurve.c"
+            "thirdParty/lmfit/lib/lmmin.c"
+            "thirdParty/lmfit/lib/lminvert.c")
+
+    add_library(lmfit ${src_files})
+    set(TOMATO_LIBS_TO_LINK ${TOMATO_LIBS_TO_LINK} lmfit)
+
 endif(USE_LMFIT)
