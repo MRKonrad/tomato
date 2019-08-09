ba1145dc9c797dfc4b4ce565f305825ef069e384
Konrad Werys <konradwerys2@gmail.com>
2019-08-09 10:51:21 +0200

    build: itk binaries download from website not from github releases

--- cmake/lmfit.cmake
+++ cmake/lmfit.cmake
@@ -1,5 +1,5 @@
-#if(USE_LMFIT)
-#    add_subdirectory(thirdParty/lmfit)
+if(USE_LMFIT)
+    add_subdirectory(thirdParty/lmfit)
 #    include_directories (${lmfit_SOURCE_DIR}/lib)
 #    set(TOMATO_LIBS_TO_LINK ${TOMATO_LIBS_TO_LINK} lmfit)
 #    set(LIB_INSTALL OFF)
@@ -9,4 +9,4 @@
 #            RUNTIME DESTINATION ${CMAKE_INSTALL_PREFIX}/${LIB_INSTALL_DIR}
 #            LIBRARY DESTINATION ${CMAKE_INSTALL_PREFIX}/${LIB_INSTALL_DIR}
 #            ARCHIVE DESTINATION ${CMAKE_INSTALL_PREFIX}/${LIB_INSTALL_DIR})
-#endif(USE_LMFIT)
+endif(USE_LMFIT)
--- scriptsBuild/downloadITK_linux_osx.sh
+++ scriptsBuild/downloadITK_linux_osx.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-#TODO: do the same for windows
+# TODO: download itk binaries from https://github.com/MRKonrad/build-itk. Problem:
+# /usr/bin/ld: /home/travis/build/MRKonrad/ITK_install/lib/libitksys-4.13.a(SystemTools.cxx.o): unrecognized relocation (0x2a) in section `.text'
+# /usr/bin/ld: final link failed: Bad value
 
 export mysystem=`uname`
 
@@ -11,10 +13,15 @@ fi
 
 echo ${mysystem}
 cd ../..
-mkdir ITK_install
-cd ITK_install
-curl -L -O https://github.com/MRKonrad/build-itk/releases/download/itk_v4.13.2/itk_v4.13.2_${mysystem}_g++_CXX98.zip
-unzip -a -q itk_v4.13.2_${mysystem}_g++_CXX98.zip
-rm itk_v4.13.2_${mysystem}_g++_CXX98.zip
+
+#mkdir ITK_install
+#cd ITK_install
+#curl -L -O https://github.com/MRKonrad/build-itk/releases/download/itk_v4.13.2/itk_v4.13.2_${mysystem}_g++_CXX98.zip
+#unzip -a -q itk_v4.13.2_${mysystem}_g++_CXX98.zip
+#rm itk_v4.13.2_${mysystem}_g++_CXX98.zip
+
+curl -L -O http://mrkonrad.github.io/MRKonrad/files/ITK_built/ITK413_${mysystem}_install.zip
+unzip -a -q ITK413_${mysystem}_install.zip
+rm ITK413_${mysystem}_install.zip
 
 echo "ITK binaries downloaded"
