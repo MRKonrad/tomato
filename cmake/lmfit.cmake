cf82fda75376a4db5d0fa44b08cb0cef61937a68
Konrad Werys <konradwerys2@gmail.com>
2019-08-09 11:31:39 +0200

    build: adding lmfit v2

--- cmake/lmfit.cmake
+++ cmake/lmfit.cmake
@@ -5,6 +5,9 @@ if(USE_LMFIT)
     #    include_directories (${lmfit_SOURCE_DIR}/lib)
     #    set(TOMATO_LIBS_TO_LINK ${TOMATO_LIBS_TO_LINK} lmfit)
 
+    project(lmfit LANGUAGES C)
+    set(CMAKE_C_STANDARD 99)
+
     include_directories ("thirdParty/lmfit/lib")
     set(src_files
             "thirdParty/lmfit/lib/lmcurve.c"
