a055d0ef635a0e38feacb2279a9d7928f58ae35f
Konrad Werys <konradwerys2@gmail.com>
2019-08-09 10:41:39 +0200

    build: something is wrong v4

--- cmake/lmfit.cmake
+++ cmake/lmfit.cmake
@@ -1,12 +1,12 @@
-if(USE_LMFIT)
-    add_subdirectory(thirdParty/lmfit)
-    include_directories (${lmfit_SOURCE_DIR}/lib)
-    set(TOMATO_LIBS_TO_LINK ${TOMATO_LIBS_TO_LINK} lmfit)
-    set(LIB_INSTALL OFF)
-    set(LIB_MAN OFF)
-    set(FITTEST OFF)
-    install (TARGETS lmfit
-            RUNTIME DESTINATION ${CMAKE_INSTALL_PREFIX}/${LIB_INSTALL_DIR}
-            LIBRARY DESTINATION ${CMAKE_INSTALL_PREFIX}/${LIB_INSTALL_DIR}
-            ARCHIVE DESTINATION ${CMAKE_INSTALL_PREFIX}/${LIB_INSTALL_DIR})
-endif(USE_LMFIT)
+#if(USE_LMFIT)
+#    add_subdirectory(thirdParty/lmfit)
+#    include_directories (${lmfit_SOURCE_DIR}/lib)
+#    set(TOMATO_LIBS_TO_LINK ${TOMATO_LIBS_TO_LINK} lmfit)
+#    set(LIB_INSTALL OFF)
+#    set(LIB_MAN OFF)
+#    set(FITTEST OFF)
+#    install (TARGETS lmfit
+#            RUNTIME DESTINATION ${CMAKE_INSTALL_PREFIX}/${LIB_INSTALL_DIR}
+#            LIBRARY DESTINATION ${CMAKE_INSTALL_PREFIX}/${LIB_INSTALL_DIR}
+#            ARCHIVE DESTINATION ${CMAKE_INSTALL_PREFIX}/${LIB_INSTALL_DIR})
+#endif(USE_LMFIT)
