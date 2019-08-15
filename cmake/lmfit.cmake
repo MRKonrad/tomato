4d0e8f476230acb3c1b0f38f409436a365bcbee6
Konrad Werys <konradwerys2@gmail.com>
2019-08-15 20:27:02 +0200

    build: configuring lmfit in cmake

--- cmake/lmfit.cmake
+++ cmake/lmfit.cmake
@@ -1,20 +1,7 @@
 if(USE_LMFIT)
 
-    # this way brings unexplained cmake errors
-    #    add_subdirectory(thirdParty/lmfit)
-    #    include_directories (${lmfit_SOURCE_DIR}/lib)
-    #    set(TOMATO_LIBS_TO_LINK ${TOMATO_LIBS_TO_LINK} lmfit)
-
-    project(lmfit LANGUAGES C)
-    set(CMAKE_C_STANDARD 99)
-
-    include_directories ("thirdParty/lmfit/lib")
-    set(src_files
-            "thirdParty/lmfit/lib/lmcurve.c"
-            "thirdParty/lmfit/lib/lmmin.c"
-            "thirdParty/lmfit/lib/lminvert.c")
-
-    add_library(lmfit ${src_files})
+    include_directories ("thirdParty/lmfit/include")
+    link_directories(${CMAKE_SOURCE_DIR}/thirdParty/lmfit/lib)
     set(TOMATO_LIBS_TO_LINK ${TOMATO_LIBS_TO_LINK} lmfit)
 
 endif(USE_LMFIT)
