34a3bff9b3f4126b07fdbbff3ad102d7f8f30701
Konrad Werys <konradwerys2@gmail.com>
2019-08-16 13:01:08 +0200

    build: installing lmfit in windows

--- scriptsBuild/downloadLmfit_windows.bat
+++ scriptsBuild/downloadLmfit_windows.bat
@@ -1,7 +1,7 @@
 cd ../thirdParty
 mkdir lmfit
 cd lmfit
-curl -L https://github.com/MRKonrad/build-lmfit/releases/download/lmfit_master/lmfit_master_windows_Win32_v120.zip -o lmfit.zip
+curl -L https://github.com/MRKonrad/build-lmfit/releases/download/lmfit_master/lmfit_master_windows_%PLATFORM%_v120.zip -o lmfit.zip
 7z x -r lmfit.zip
 del lmfit.zip
 
