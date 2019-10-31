84300a517923649c367bcb17cb3635da3580e5c6
Konrad Werys <konradwerys2@gmail.com>
2019-10-31 16:46:01 +0100

    ci: big changes to the builds

--- scriptsBuild/downloadLmfit_windows.bat
+++
@@ -1,8 +0,0 @@
-cd ../thirdParty
-mkdir lmfit
-cd lmfit
-curl -L https://github.com/MRKonrad/build-lmfit/releases/download/lmfit_master/lmfit_master_windows_%PLATFORM%_v120.zip -o lmfit.zip
-7z x -r lmfit.zip
-del lmfit.zip
-
-echo "lmfit binaries downloaded"
