e3f0532958ffabd359be5062ad3c3b605b902254
Konrad Werys <konradwerys2@gmail.com>
2019-11-28 13:25:49 +0100

    feat: one pixel calculation with toamtooptions v9

--- cmake/libyaml.cmake
+++ cmake/libyaml.cmake
@@ -1,6 +1,7 @@
 if(USE_YAML)
 
     set (BUILD_TESTING OFF CACHE INTERNAL "" FORCE)
+    set (BUILD_SHARED_LIBS ON CACHE INTERNAL "" FORCE)
     add_subdirectory (thirdParty/libyaml)
     include_directories (${yaml_BINARY_DIR}/include ${yaml_SOURCE_DIR}/include) # for windows
     set(TOMATO_LIBS_TO_LINK ${TOMATO_LIBS_TO_LINK} yaml)
