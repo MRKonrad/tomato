c150b5868ec7d7cc3995d21d998b878afb4d818b
Travis CI <travis@travis-ci.org>
2021-04-23 08:38:00 +0000

    Deploy code docs to GitHub Pages Travis build: 649
    Commit: 021a4ca607b3f8ec4f00b46459293c297aaaba1c

---
+++ html/class_ox_1_1_model_t1_two_param.js
@@ -0,0 +1,11 @@
+var class_ox_1_1_model_t1_two_param =
+[
+    [ "ModelT1TwoParam", "class_ox_1_1_model_t1_two_param.html#a307993e56c9735f14d23462526eabd82", null ],
+    [ "~ModelT1TwoParam", "class_ox_1_1_model_t1_two_param.html#ae6870d10db397016649989969c613f32", null ],
+    [ "calcCostDerivative", "class_ox_1_1_model_t1_two_param.html#a328e0fdc6b5769ecc3327596c9b12be3", null ],
+    [ "calcCostValue", "class_ox_1_1_model_t1_two_param.html#ab4ec672167094e84f2fddc5d052d528c", null ],
+    [ "calcLSJacobian", "class_ox_1_1_model_t1_two_param.html#af5952a47062e6edffe78f8594cc220e4", null ],
+    [ "calcLSResiduals", "class_ox_1_1_model_t1_two_param.html#a8c02d34fac5a35310f6ee60209f70d45", null ],
+    [ "calcModelValue", "class_ox_1_1_model_t1_two_param.html#aaa8218e3d53e1913589d272bcbb62b47", null ],
+    [ "newByCloning", "class_ox_1_1_model_t1_two_param.html#aa090c6834141f00a966eebd6b0415e44", null ]
+];
